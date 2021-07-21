222ae258e12dd7101bb958769afc977e5fd01823
manav-1 <manav81101@gmail.com>
2021-07-21 23:13:58 +0530

    commit

--- src/components/screens/Landing.js
+++ src/components/screens/Landing.js
@@ -3,37 +3,76 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import logo from "../../assets/logo.png";
 import team from "../../assets/team.png";
 import $ from "jquery"; // eslint-disable-line
+import { Popper } from "react-popper"; // eslint-disable-line
+// import {Navbar, Container, NavDropdown} from 'react-bootstrap'
 import "bootstrap/dist/js/bootstrap.bundle.min";
+import { useState } from "react";
 
 export default function Landing({ navigation }) {
+  const [isNavCollapsed, setIsNavCollapsed] = useState(true);
+
+  const handleNavCollapse = () => setIsNavCollapsed(!isNavCollapsed);
+
   return (
     <div style={{ width: "100%" }}>
-      <div style={{ width: "100%" }}>
-        <nav className="navbar bg-light py-0 navbar-light fixed-top">
-          <button
-            className="navbar-brand"
-            onClick={() => navigation.navigate("Landing")}
-          >
-            <img src={logo} alt="Logo" className="header__logo" />
-          </button>
+      <nav className="navbar navbar-expand-lg bg-light py-0 navbar-light fixed-top">
+        <button
+          className="navbar-brand"
+          onClick={() => navigation.navigate("Landing")}
+        >
+          <img src={logo} alt="Logo" className="header__logo" />
+        </button>
+        <button
+          className="navbar-toggler"
+          type="button"
+          onClick={handleNavCollapse}
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
 
-          <ul className="bsnav navbar-nav d-flex flex-row">
+        <div
+          className={`${isNavCollapsed ? "collapse" : ""} navbar-collapse`}
+          id="main-nav"
+        >
+          <ul className="bsnav navbar-nav d-flex flex-column">
             <li className="nav-item pl-4">
-              <button className="nav-link">About</button>
+              <a href="#About" onClick={handleNavCollapse} className="nav-link">
+                About
+              </a>
             </li>
             <li className="nav-item pl-4">
               <button
                 className="nav-link"
-                onClick={() => navigation.navigate("Dashboard")}
+                onClick={() => {
+                  handleNavCollapse();
+                  navigation.navigate("Dashboard");
+                }}
               >
                 Placement Portal
               </button>
             </li>
             <li className="nav-item pl-4">
-              <button className="nav-link">Contact Us</button>
+              <a
+                href="#Faculty"
+                onClick={handleNavCollapse}
+                className="nav-link"
+              >
+                Faculty
+              </a>
+            </li>
+            <li className="nav-item pl-4">
+              <a
+                href="#contact"
+                onClick={handleNavCollapse}
+                className="nav-link"
+              >
+                Contact Us
+              </a>
             </li>
           </ul>
-        </nav>
+        </div>
+      </nav>
+      <div style={{ width: "100%", margin: "auto" }}>
         <header style={{ width: "100%" }} className="header mt-5">
           <div className="header__text-box">
             <h1 className="heading-primary">
@@ -50,7 +89,11 @@ export default function Landing({ navigation }) {
           </div>
         </header>
       </div>
-      <div style={{ width: "100%" }} className="landing-about row">
+      <div
+        id="About"
+        style={{ width: "100%", margin: "auto" }}
+        className="landing-about row"
+      >
         <h1 className="col-sm-12">About Us</h1>
         <div className="about col-sm-6">
           <h3 className="heading-tert margin-bottom-small">Our Aim</h3>
@@ -78,7 +121,11 @@ export default function Landing({ navigation }) {
           <img style={{ width: "100%" }} src={team} alt="Team" />
         </div>
       </div>
-      <div style={{ width: "100%" }} className="landing-about row">
+      <div
+        id="Faculty"
+        style={{ width: "100%", margin: "auto" }}
+        className="landing-about row"
+      >
         <h1 className="col-sm-12">Faculty</h1>
         <div
           id="carouselExampleIndicators"
@@ -86,35 +133,44 @@ export default function Landing({ navigation }) {
           data-ride="carousel"
           style={{ width: "90%", margin: "auto" }}
         >
-          <ol className="carousel-indicators">
-            <li
-              data-target="#carouselExampleIndicators"
-              data-slide-to="0"
-              className="active"
-            ></li>
-            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-          </ol>
           <div className="carousel-inner">
             <div className="carousel-item active">
               <img className="d-block w-100" src={team} alt="First slide" />
-              <div class="carousel-caption d-none d-md-block bg-dark">
-                <h5>Convener</h5>
-                <p>Mr Subodh Pandit</p>
+              <div className="d-flex flex-direction-row justify-content-around bg-dark text-center text-white">
+                <div>
+                  <h5>Convener</h5>
+                  <p>Mr Subodh Pandit</p>
+                </div>
+                <div>
+                  <h5>Convener</h5>
+                  <p>Mr Subodh Pandit</p>
+                </div>
               </div>
             </div>
             <div className="carousel-item">
               <img className="d-block w-100" src={team} alt="Second slide" />
-              <div class="carousel-caption d-none d-md-block bg-dark">
-                <h5>Convener</h5>
-                <p>Mr Subodh Pandit</p>
+              <div className="d-flex flex-direction-row justify-content-around bg-dark text-center text-white">
+                <div>
+                  <h5>Convener</h5>
+                  <p>Mr Subodh Pandit</p>
+                </div>
+                <div>
+                  <h5>Convener</h5>
+                  <p>Mr Subodh Pandit</p>
+                </div>
               </div>
             </div>
             <div className="carousel-item">
               <img className="d-block w-100" src={team} alt="Third slide" />
-              <div class="carousel-caption d-none d-md-block bg-dark">
-                <h5>Convener</h5>
-                <p>Mr Subodh Pandit</p>
+              <div className="d-flex flex-direction-row justify-content-around bg-dark text-center text-white">
+                <div>
+                  <h5>Convener</h5>
+                  <p>Mr Subodh Pandit</p>
+                </div>
+                <div>
+                  <h5>Convener</h5>
+                  <p>Mr Subodh Pandit</p>
+                </div>
               </div>
             </div>
           </div>
@@ -144,6 +200,36 @@ export default function Landing({ navigation }) {
           </a>
         </div>
       </div>
+      <footer id="contact" className="text-center py-5">
+        <div className="social">
+          <a
+            href="https://www.instagram.com/placementcell_kmv/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <i className="fa fa-instagram"></i>
+          </a>
+          <a
+            href="https://www.linkedin.com/in/keshavmahavidyalaya/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <i className="fa fa-linkedin"></i>
+          </a>
+          <p className="copyright text-center">
+            <b>Start@KMV, The Placement Cell</b>
+            <br />
+            Keshav Mahavidyalaya
+            <br />
+            University of Delhi
+          </p>
+          <p className="copyright mt-5 mb-0">
+            Managed by
+            <br />
+            <a href="https://github.com/manav-1">Manav Arora</a>
+          </p>
+        </div>
+      </footer>
     </div>
   );
 }
