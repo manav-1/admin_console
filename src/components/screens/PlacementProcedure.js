83455ca3c0f99db32b56d7f37c65175c24f4562a
manav-1 <manav81101@gmail.com>
2021-08-06 13:05:14 +0530

    Placement Procedure

--- src/components/screens/PlacementProcedure.js
+++ src/components/screens/PlacementProcedure.js
@@ -1,8 +1,36 @@
 import React from "react"; //eslint-disable-line
+import { Dimensions } from "react-native";
 import logo from "../../assets/logo.png";
 import "../css/procedure.css";
+import { Chrono } from "react-chrono";
 
 export default function PlacementProcedure({ navigation }) {
+  const items = [
+    {
+      title: "01",
+      cardTitle: "Company Details",
+      cardDetailedText:
+        "Post accepting the placement invite, the recruiter shall share the details of the profile and job description along with compensation details with the Placement Team.",
+    },
+    {
+      title: "02",
+      cardTitle: "Inviting Applications",
+      cardDetailedText:
+        " A coordinator is assigned to the drive, who circulates the opportunity and collects the required data of the desiring candidates and shares it with the recruiter.",
+    },
+    {
+      title: "03",
+      cardTitle: "Pre-Placement Talk and Selection Procedure",
+      cardDetailedText:
+        "If a company wishes, it may allot a day for a Pre-Placement Talk with the candidates to acquaint the candidates about the company's profile, organizational achievements and culture of the company. Further, the coordinator ensures a smooth conduct of various rounds, including but not limited to Aptitude test, Group discussion, Technical and Personal interviews",
+    },
+    {
+      title: "04",
+      cardTitle: "Job Offer",
+      cardDetailedText:
+        "After the successful completion of all rounds, the company informs the Placement Team about the selected applicants, and if the company so wishes, the Cell conveys the offer to the selected students",
+    },
+  ];
   return (
     <div className="procedure">
       <button
@@ -13,64 +41,14 @@ export default function PlacementProcedure({ navigation }) {
       </button>
       <h1 className="heading-secondary">Placement Procedure</h1>
       <div className="procedure-container">
-        <ul>
-          <li>
-            <div>
-              <h3 className="lightgray font-weight-bold">
-                01 <br />
-                Company Details
-              </h3>
-              <p>
-                Post accepting the placement invite, the recruiter shall share
-                the details of the profile and job description along with
-                compensation details with the Placement Team.
-              </p>
-            </div>
-          </li>
-          <li>
-            <div>
-              <h3 className="lightgray font-weight-bold">
-                02 <br />
-                Inviting Applications
-              </h3>
-              <p>
-                A coordinator is assigned to the drive, who circulates the
-                opportunity and collects the required data of the desiring
-                candidates and shares it with the recruiter.
-              </p>
-            </div>
-          </li>
-          <li>
-            <div>
-              <h3 className="lightgray font-weight-bold">
-                03 <br />
-                PRE-PLACEMENT TALK AND SELECTION PROCEDURE
-              </h3>
-              <p>
-                If a company wishes, it may allot a day for a Pre-Placement Talk
-                with the candidates to acquaint the candidates about the
-                company's profile, organizational achievements and culture of
-                the company. Further, the coordinator ensures a smooth conduct
-                of various rounds, including but not limited to Aptitude test,
-                Group discussion, Technical and Personal interviews
-              </p>
-            </div>
-          </li>
-          <li>
-            <div>
-              <h3 className="lightgray font-weight-bold">
-                04 <br />
-                Job Offer
-              </h3>
-              <p>
-                After the successful completion of all rounds, the company
-                informs the Placement Team about the selected applicants, and if
-                the company so wishes, the Cell conveys the offer to the
-                selected students
-              </p>
-            </div>
-          </li>
-        </ul>
+        <Chrono
+          items={items}
+          mode={
+            Dimensions.get("screen").width > 768
+              ? "VERTICAL_ALTERNATING"
+              : "VERTICAL"
+          }
+        />
       </div>
     </div>
   );
